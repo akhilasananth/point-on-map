3921ddd806594ecf9c378d15d3ae88ef03d2eabb
akhilasananth <akhilasananth@gmail.com>
2016-05-09 13:27:24 -0400

    ,akes path between two points

--- MapsTest/MapsTest/Form1.cs
+++ MapsTest/MapsTest/Form1.cs
@@ -13,9 +13,13 @@ namespace MapsTest
 {
     public partial class Form1 : Form
     {
-        GMap.NET.WindowsForms.GMapOverlay overlayOne;
+        static GMap.NET.PointLatLng start = new GMap.NET.PointLatLng(40, -83);
+        static GMap.NET.PointLatLng end = new GMap.NET.PointLatLng(37, -115);
+
+
         public Form1()
         {
+
             InitializeComponent();
 
             var myMap = gMapControl1;
@@ -26,13 +30,21 @@ namespace MapsTest
             myMap.Zoom = 4;
             myMap.Manager.Mode = GMap.NET.AccessMode.ServerAndCache;
 
-            overlayOne = new GMap.NET.WindowsForms.GMapOverlay( "markers");
-            overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new GMap.NET.PointLatLng(40, -83), GMarkerGoogleType.green));
+            //Puts points on the map
+            GMap.NET.WindowsForms.GMapOverlay overlayOne = new GMap.NET.WindowsForms.GMapOverlay("markers");
+            overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(start, GMarkerGoogleType.green));
+            overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMarkerGoogle(end, GMarkerGoogleType.green));
             myMap.Overlays.Add(overlayOne);
+
+            //Connects two points on the map
+            GMap.NET.MapRoute route = GMap.NET.MapProviders.BingMapProvider.Instance.GetRoute(start, end, false, false, 15);
+            GMap.NET.WindowsForms.GMapRoute r = new GMap.NET.WindowsForms.GMapRoute(route.Points, "My route");
+
+            GMap.NET.WindowsForms.GMapOverlay routesOverlay = new GMap.NET.WindowsForms.GMapOverlay("routes");
+            routesOverlay.Routes.Add(r);
+            myMap.Overlays.Add(routesOverlay);
+        }
 
 
     }
-
-
-}
 }
